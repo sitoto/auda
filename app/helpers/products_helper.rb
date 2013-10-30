c23176eacf2581660fdd2efda3a6ebbb05bd2ffa
sitoto <huang.gadela@gmail.com>
2013-10-30 18:03:43 +0800

    product new ; dynamic form

--- app/helpers/products_helper.rb
+++ app/helpers/products_helper.rb
@@ -1,2 +1,6 @@
 module ProductsHelper
+  def render_product_category_select_tag(product)
+    return if product.blank?
+    collection_select :product, :category_id, Category.all, :id, :name, {:prompt => t('categories.select')}, :style => "width:145px;"
+  end
 end
