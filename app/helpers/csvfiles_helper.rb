d241a42800e14dd1f6a2c46e03a9f25be9560f14
sitoto <huang.gadela@gmail.com>
2013-11-18 15:43:43 +0800

    some nav

--- app/helpers/csvfiles_helper.rb
+++ app/helpers/csvfiles_helper.rb
@@ -6,5 +6,11 @@ module CsvfilesHelper
     
     collection_select :pairparameters, property.id, parameters, :name, :name, {:prompt => t('pairs.select')}, :style => "width:145px;"
   end
+
+  def render_csvfile_status_tag(status)
+    return t('csvfiles.pipei') if status == 1
+    return t('csvfiles.unpipei') if status == 0
+
+  end
  
 end
