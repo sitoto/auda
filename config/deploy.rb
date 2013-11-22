bcb690491e7747ebae30b7982cda373d633a91d6
sitoto <huang.gadela@gmail.com>
2013-11-22 18:57:15 +0800

    deploy to uniron and nignx

---
+++ config/deploy.rb
@@ -0,0 +1,65 @@
+# coding: utf-8
+require "bundler/capistrano"
+require "rvm/capistrano"
+
+default_run_options[:pty] = true
+
+set :rvm_ruby_string, 'ruby-2.0.0-p247'
+set :rvm_type, :user
+set :application, "auda"
+set :repository,  "git://github.com/sitoto/auda.git"
+set :branch, "master"
+set :scm, :git
+set :user, "ruby"
+set :deploy_to, "/data/www/#{application}"
+set :runner, "ruby"
+# set :deploy_via, :remote_cache
+set :git_shallow_clone, 1
+
+role :web, "192.168.2.21"                          # Your HTTP server, Apache/etc
+role :app, "192.168.2.21"                          # This may be the same as your `Web` server
+role :db,  "192.168.2.21", :primary => true # This is where Rails migrations will run
+
+# unicorn.rb 路径
+set :unicorn_path, "#{deploy_to}/current/config/unicorn.rb"
+
+namespace :deploy do
+  task :start, :roles => :app do
+    run "cd #{deploy_to}/current/; RAILS_ENV=production bundle exec unicorn_rails -c #{unicorn_path} -D"
+  end
+
+  task :stop, :roles => :app do
+    run "kill -QUIT `cat #{deploy_to}/current/tmp/pids/unicorn.pid`"
+  end
+
+  desc "Restart Application"
+  task :restart, :roles => :app do
+    run "kill -USR2 `cat #{deploy_to}/current/tmp/pids/unicorn.pid`"
+  end
+end
+
+task :init_shared_path, :roles => :web do
+  run "mkdir -p #{deploy_to}/shared/log"
+  run "mkdir -p #{deploy_to}/shared/pids"
+  run "mkdir -p #{deploy_to}/shared/assets"
+end
+
+task :link_shared_files, :roles => :web do
+  run "ln -sf #{deploy_to}/shared/config/*.yml #{deploy_to}/current/config/"
+  run "ln -sf #{deploy_to}/shared/config/unicorn.rb #{deploy_to}/current/config/"
+  run "ln -sf #{deploy_to}/shared/config/initializers/secret_token.rb #{deploy_to}/current/config/initializers"
+end
+
+task :mongoid_create_indexes, :roles => :web do
+  run "cd #{deploy_to}/current/; RAILS_ENV=production bundle exec rake db:mongoid:create_indexes"
+end
+
+task :compile_assets, :roles => :web do
+  run "cd #{deploy_to}/current/; RAILS_ENV=production bundle exec rake assets:precompile"
+end
+
+task :mongoid_migrate_database, :roles => :web do
+  run "cd #{deploy_to}/current/; RAILS_ENV=production bundle exec rake db:migrate"
+end
+
+after "deploy:finalize_update","deploy:symlink", :init_shared_path, :link_shared_files, :compile_assets,  :mongoid_migrate_database
