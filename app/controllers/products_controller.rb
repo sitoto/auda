02f4a14252fca02a5bb6691522f74d8aae11faad
sitoto <huang.gadela@gmail.com>
2013-12-02 17:55:37 +0800

    add layout in products

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,6 +1,7 @@
 class ProductsController < ApplicationController
   before_action :set_product, only: [:edit, :update, :show, :agree]
   load_and_authorize_resource except: [:create]
+  layout "main"
 
   def index
     @category = Category.find(params[:category_id])
