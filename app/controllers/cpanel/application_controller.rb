76e56444176cd06378539acf9ba9d144a6f220c0
sitoto <huang.gadela@gmail.com>
2013-12-19 14:16:50 +0800

    rols

--- app/controllers/cpanel/application_controller.rb
+++ app/controllers/cpanel/application_controller.rb
@@ -4,7 +4,7 @@ class Cpanel::ApplicationController < ApplicationController
   before_filter :require_admin
 
   def require_admin
-    unless current_user.has_role?("admin") || current_user.email.eql?('huangw@jiaparts.com')
+    unless current_user.admin? || current_user.email.eql?('huangw@jiaparts.com')
       flash[:danger] = t("cancanerror")
       redirect_to root_url
     end
