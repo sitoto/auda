4831a7035ccc2903c1491c5522e4b1975f062bc4
sitoto <huang.gadela@gmail.com>
2014-01-08 17:15:55 +0800

    ability's commont

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -3,6 +3,24 @@ class Ability
 
   def initialize(user)
     user ||= User.new
+=begin
+    user.permissions.each do |p|
+      begin
+        action = p.action.to_sym
+        subject = begin
+                    # RESTful Controllers
+                    p.subject.camelize.constantize
+                  rescue
+                    # Non RESTful Controllers
+                    p.subject.underscore.to_sym
+                  end
+        can action, subject
+      rescue => e
+        Rails.logger.info "#{e}"
+        Rails.logger.info "#{subject}"
+      end
+    end
+=end
 
     if !user.blank?
       #cannot :manage , :all
@@ -79,6 +97,6 @@ class Ability
   end
   protected
   def basic_read_only
-#    can :node, Category
+    #    can :node, Category
   end
 end
