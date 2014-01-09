0a17ff246e3f053b672d70c6fdcd03c053dda693
sitoto <huang.gadela@gmail.com>
2014-01-09 15:15:12 +0800

    can load image from gridfs_controller

--- app/controllers/gridfs_controller.rb
+++ app/controllers/gridfs_controller.rb
@@ -4,9 +4,14 @@ class GridfsController < ApplicationController
   def serve
     gridfs_path = env["PATH_INFO"].gsub("/images/", "")
     begin
-      gridfs_file = Mongo::GridFileSystem.new(Mongoid.database).open(gridfs_path, 'r')
-      self.response_body = gridfs_file.read
+      #gridfs_file = Mongo::GridFileSystem.new(Mongoid.database).open(gridfs_path, 'r')
+      #self.response_body = gridfs_file.read
+      #self.content_type = gridfs_file.content_type
+
+      gridfs_file = Mongoid::GridFS[gridfs_path]
+      self.response_body = gridfs_file.data
       self.content_type = gridfs_file.content_type
+
     rescue
       self.status = :file_not_found
       self.content_type = 'text/plain'
