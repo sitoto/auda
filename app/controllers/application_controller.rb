4e92ac985559c1b727041115b2cb68cce8475e5a
sitoto <huang.gadela@gmail.com>
2014-02-13 13:33:38 +0800

    8181

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,25 @@
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
+  before_filter :use_special_port if Rails.env == 'production'
 
   protect_from_forgery with: :exception
 
   rescue_from CanCan::AccessDenied do |exception|
     redirect_to main_app.root_url, :notice => t('cancanerror') #exception.message
   end
+  def use_special_port
+    class << request
+      def port 
+        if remote_ip.eq?('server')
+          80
+        else
+          8181 
+        end
+      end
+    end
+  end
+
 
   #  before_filter do
   #    resource = controller_name.singularize.to_sym
