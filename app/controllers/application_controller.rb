4125efac4a1ab0beb07e845a1acaf93c08988ea0
sitoto <huang.gadela@gmail.com>
2014-02-19 10:19:21 +0800

    remove 8181

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
-  before_filter :use_special_port if Rails.env == 'production'
+#  before_filter :use_special_port if Rails.env == 'production'
 
   protect_from_forgery with: :exception
 
